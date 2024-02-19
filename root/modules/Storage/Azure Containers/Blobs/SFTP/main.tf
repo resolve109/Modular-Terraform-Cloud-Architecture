1942a145f9d821f114d8d45fbd59090a4ff835c5
Sid Patel <sidpatel868@gmail.com>
2024-02-19 14:44:58 -0500

    Update main.tf

--- root/modules/Storage/Azure Containers/Blobs/SFTP/main.tf
+++ root/modules/Storage/Azure Containers/Blobs/SFTP/main.tf
@@ -1,79 +1,134 @@
-#Storage Account  - name # if you want to use the same storage account,
-# you can use the same name, but you need to change the resource group name
-
-#Storage Account  - name
-
-resource "azurerm_storage_account" "stracc1" { #"stracc1" is the name of the resource
-    name                     = "nameastupidaccount"
-    resource_group_name      = azurerm_resource_group.example.name
-    location                 = azurerm_resource_group.example.location
-    account_tier             = "Standard"
-    account_replication_type = "GRS"
-
-    tags = {
-        environment = "staging"
-    }
-
-
+# Resource group creation
+resource "azurerm_resource_group" "my_resource_group" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-#Storage Account  - container
-resource "azurerm_storage_container" "strcont1" { #"strcont1" is the name of the resource
-  name                  = "nameacontainerstupid"
-  storage_account_name  = azurerm_storage_account.strcont1.name
+# Storage account creation
+resource "azurerm_storage_account" "strgacc1" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.my_resource_group.name
+  location                 = azurerm_resource_group.my_resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Storage container creation
+resource "azurerm_storage_container" "strgacccont1" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
 }
 
-#Storage Account  - blob
-resource "azurerm_storage_blob" "strblob1" { # "strblob1" is the name of the resource
-  name                   = "nameablobstupid"
-  storage_account_name   = azurerm_storage_account.stracc1.name # Required - the name of the storage account
-  storage_container_name = azurerm_storage_container.strcont1.name  # Required - the name of the container in which the blob should be created
-  type                   = "Block" # Required - the type of blob to create - Block or Page
-  source                 = "example.txt" # Path to the file to be uploaded
-} 
-
-#Storage Account  - blob
-resource "azurerm_storage_blob" "strblob2" { # "strblob2" is the name of the resource
-  name                   = "nameablobstupid2" # Required - the name of the blob
-  storage_account_name   = azurerm_storage_account.stracc1.name # Required - the name of the storage account
-  storage_container_name = azurerm_storage_container.strcont1.name # Required - the name of the container in which the blob should be created
-  type                   = "Block" # Required - the type of blob to create - Block or Page
-  source                 = "example2.txt" # Path to the file to be uploaded
+# Storage share creation
+resource "azurerm_storage_share" "strgshare1" {
+  name                 = var.storage_share_name
+  storage_account_name = azurerm_storage_account.example.name
+  quota                = 50
 }
 
-data "azurerm_storage_account_sas" "sastracc1" { # "sastracc1" is the name of the resource
-    connection_string = azurerm_storage_account.stracc1.primary_connection_string   # Required - the connection string for the storage account
-    https_only        = true # Optional - defaults to true
- 
-    resource_types {  # Optional - if not specified, all resource types are included
-        service   = true # Allow access to the service - required for container and object level access
-        container = false # Allow access to the container level - required for object level access
-        object    = false  # Allow access to the object level - required for object level access
-    }
-
-    services {
-        blob  = true  # Allow access to the blob service - required for container and object level access
-        queue = false # Allow access to the queue service - only valid for standard storage accounts
-        table = false # Allow access to the table service - only valid for classic storage accounts
-        file  = false # Allow access to the file service - only valid for premium storage accounts 
-    }
-
-    start  = "2020-01-01" # Date in the past - required for the expiry to be valid - format YYYY-MM-DD 
-    expiry = "2022-01-01" # Date in the future - required for the expiry to be valid - format YYYY-MM-DD
-
-    permissions {
-        tag     = "example_tag" # Optional tag
-        filter  = "example_filter" # Optional filter - service version
-        read    = true # Allow read access - applies to container and blob
-        write   = true # Allow write access - applies to container and blob
-        delete  = false # Allow delete access - applies to container and blob
-        list    = true  # Allow list access - applies to container and blob 
-        add     = true # Allow add access - applies to container and blob
-        create  = true # Allow create access (add and write) - applies to container and blob 
-        update  = false
-        process = false
-    }
+# Storage directory creation
+resource "azurerm_storage_directory" "strgdir1" {
+  name                 = var.storage_directory_name
+  storage_account_name = azurerm_storage_account.example.name
+  share_name           = azurerm_storage_share.example.name
 }
 
+# Storage file creation
+resource "azurerm_storage_file" "strgfile1" {
+  name                 = var.storage_file_name
+  storage_account_name = azurerm_storage_account.example.name
+  share_name           = azurerm_storage_share.example.name
+  directory_name       = azurerm_storage_directory.example.name
+  source               = var.local_file_path
+}
 
+# Virtual network creation
+resource "azurerm_virtual_network" "strgvnet1" {
+  name                = var.virtual_network_name
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
+}
+
+# Subnet creation
+resource "azurerm_subnet" "strgsub1" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.my_resource_group.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Key vault creation
+resource "azurerm_key_vault" "strgkv1" {
+  name                        = var.key_vault_name
+  resource_group_name         = azurerm_resource_group.my_resource_group.name
+  location                    = azurerm_resource_group.my_resource_group.location
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+}
+
+# Key vault access policy creation
+resource "azurerm_key_vault_access_policy" "example" {
+  key_vault_id = azurerm_key_vault.example.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = var.object_id
+
+  certificate_permissions = [
+    "get",
+    "list",
+    "create",
+    "import",
+    "delete",
+    "update",
+    "backup",
+    "restore",
+  ]
+
+  secret_permissions = [
+    "get",
+    "list",
+    "set",
+    "delete",
+    "backup",
+    "restore",
+  ]
+
+  storage_permissions = [
+    "get",
+    "list",
+    "delete",
+    "set",
+    "update",
+    "regeneratekey",
+    "recover",
+    "purge",
+  ]
+
+  key_permissions = [
+    "get",
+    "list",
+    "create",
+    "import",
+    "delete",
+    "update",
+    "backup",
+    "restore",
+    "recover",
+    "purge",
+  ]
+}
+
+# SFTP server creation
+resource "azurerm_sftp_server" "example" {
+  name                 = var.sftp_server_name
+  resource_group_name  = azurerm_resource_group.my_resource_group.name
+  location             = azurerm_resource_group.my_resource_group.location
+  storage_account_name = azurerm_storage_account.example.name
+  storage_share_name   = azurerm_storage_share.example.name
+  storage_directory_id = azurerm_storage_directory.example.id
+  ssh_public_key       = file("~/.ssh/id_rsa.pub")
+  vnet_subnet_id       = azurerm_subnet.example.id
+  key_vault_id         = azurerm_key_vault.example.id
+}
